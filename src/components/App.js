f380eda6bfd05416cb2242fd9204dc2b1be3d4fc
Puja Srivastava <srivastavapuja5@gmail.com>
2018-12-04 22:50:41 +0530

    Master : first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React , {component} from 'react';
+import '../styles/App.css';
+
+export default class App extends React.Component {
+    render() {
+      return (
+        <div>
+            <h1>This is a react app</h1>
+        </div>
+      );
+    }
+  }
+
